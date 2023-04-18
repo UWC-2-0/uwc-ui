d5781a68013b9fdc2f6a31b259c1dc6d07bdcc01
nguyentheloc-310101 <nguyentheloc310101@gmail.com>
2023-04-18 12:22:04 +0700

    loc_dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Dashboard from './pages/dashboard/dashboard';
-import AppRoute from './routes/routes';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Dashboard, Auth } from "@/layouts";
 
 function App() {
   return (
-    <>
-      <AppRoute />
-    </>
+    <Routes>
+      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/auth/*" element={<Auth />} />
+      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+    </Routes>
   );
 }
 
