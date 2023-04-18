91acb672d12317563765f5cb12a633604f2ca581
issoe <twmnno@gmail.com>
2023-04-19 01:24:55 +0700

    new branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,15 @@
 const withMT = require('@material-tailwind/react/utils/withMT');
 
 module.exports = withMT({
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tw-elements/dist/plugin")
+  ],
 });
