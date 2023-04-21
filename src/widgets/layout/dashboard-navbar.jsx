f2e240c1e1401d8e364fa754954c25dd4ef8b2b0
nguyentheloc-310101 <nguyentheloc310101@gmail.com>
2023-04-21 16:05:27 +0700

    loc_dev

--- src/widgets/layout/dashboard-navbar.jsx
+++ src/widgets/layout/dashboard-navbar.jsx
@@ -1,4 +1,4 @@
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link } from 'react-router-dom';
 import {
   Navbar,
   Typography,
@@ -11,7 +11,7 @@ import {
   MenuList,
   MenuItem,
   Avatar,
-} from "@material-tailwind/react";
+} from '@material-tailwind/react';
 import {
   UserCircleIcon,
   Cog6ToothIcon,
@@ -19,55 +19,53 @@ import {
   ClockIcon,
   CreditCardIcon,
   Bars3Icon,
-} from "@heroicons/react/24/solid";
+} from '@heroicons/react/24/solid';
 import {
   useMaterialTailwindController,
   setOpenConfigurator,
   setOpenSidenav,
-} from "@/context";
+} from '@/context';
 
 export function DashboardNavbar() {
   const [controller, dispatch] = useMaterialTailwindController();
   const { fixedNavbar, openSidenav } = controller;
   const { pathname } = useLocation();
-  const [layout, page] = pathname.split("/").filter((el) => el !== "");
+  const [layout, page] = pathname.split('/').filter((el) => el !== '');
 
   return (
     <Navbar
-      color={fixedNavbar ? "white" : "transparent"}
+      color={fixedNavbar ? 'white' : 'transparent'}
       className={`rounded-xl transition-all ${
         fixedNavbar
-          ? "sticky top-4 z-40 py-3 shadow-md shadow-blue-gray-500/5"
-          : "px-0 py-1"
+          ? 'sticky top-4 z-40 py-3 shadow-md shadow-blue-gray-500/5'
+          : 'px-0 py-1'
       }`}
       fullWidth
-      blurred={fixedNavbar}
-    >
+      blurred={fixedNavbar}>
       <div className="flex flex-col-reverse justify-between gap-6 md:flex-row md:items-center">
         <div className="capitalize">
           <Breadcrumbs
             className={`bg-transparent p-0 transition-all ${
-              fixedNavbar ? "mt-1" : ""
-            }`}
-          >
+              fixedNavbar ? 'mt-1' : ''
+            }`}>
             <Link to={`/${layout}`}>
               <Typography
                 variant="small"
                 color="blue-gray"
-                className="font-normal opacity-50 transition-all hover:text-blue-500 hover:opacity-100"
-              >
+                className="font-normal opacity-50 transition-all hover:text-blue-500 hover:opacity-100">
                 {layout}
               </Typography>
             </Link>
             <Typography
               variant="small"
               color="blue-gray"
-              className="font-normal"
-            >
+              className="font-normal">
               {page}
             </Typography>
           </Breadcrumbs>
-          <Typography variant="h6" color="blue-gray">
+          <Typography
+            variant="h6"
+            color="blue-gray">
             {page}
           </Typography>
         </div>
@@ -79,37 +77,38 @@ export function DashboardNavbar() {
             variant="text"
             color="blue-gray"
             className="grid xl:hidden"
-            onClick={() => setOpenSidenav(dispatch, !openSidenav)}
-          >
-            <Bars3Icon strokeWidth={3} className="h-6 w-6 text-blue-gray-500" />
+            onClick={() => setOpenSidenav(dispatch, !openSidenav)}>
+            <Bars3Icon
+              strokeWidth={3}
+              className="h-6 w-6 text-blue-gray-500"
+            />
           </IconButton>
           <Link to="/auth/sign-in">
             <Button
               variant="text"
               color="blue-gray"
-              className="hidden items-center gap-1 px-4 xl:flex"
-            >
+              className="hidden items-center gap-1 px-4 xl:flex">
               <UserCircleIcon className="h-5 w-5 text-blue-gray-500" />
               Sign In
             </Button>
             <IconButton
               variant="text"
               color="blue-gray"
-              className="grid xl:hidden"
-            >
+              className="grid xl:hidden">
               <UserCircleIcon className="h-5 w-5 text-blue-gray-500" />
             </IconButton>
           </Link>
           <IconButton
             variant="text"
             color="blue-gray"
-            onClick={() => setOpenConfigurator(dispatch, true)}
-          >
+            onClick={() => setOpenConfigurator(dispatch, true)}>
             <Cog6ToothIcon className="h-5 w-5 text-blue-gray-500" />
           </IconButton>
           <Menu>
             <MenuHandler>
-              <IconButton variant="text" color="blue-gray">
+              <IconButton
+                variant="text"
+                color="blue-gray">
                 <BellIcon className="h-5 w-5 text-blue-gray-500" />
               </IconButton>
             </MenuHandler>
@@ -125,22 +124,20 @@ export function DashboardNavbar() {
                   <Typography
                     variant="small"
                     color="blue-gray"
-                    className="mb-1 font-normal"
-                  >
+                    className="mb-1 font-normal">
                     <strong>New message</strong> from Laur
                   </Typography>
                   <Typography
                     variant="small"
                     color="blue-gray"
-                    className="flex items-center gap-1 text-xs font-normal opacity-60"
-                  >
-                    <ClockIcon className="h-3.5 w-3.5" /> 13 minutes ago
+                    className="flex items-center gap-1 text-xs font-normal opacity-60">
+                    <ClockIcon className="h-3.5 w-3.5" /> 12 mins ago
                   </Typography>
                 </div>
               </MenuItem>
               <MenuItem className="flex items-center gap-4">
                 <Avatar
-                  src="https://demos.creative-tim.com/material-dashboard/assets/img/small-logos/logo-spotify.svg"
+                  src=""
                   alt="item-1"
                   size="sm"
                   variant="circular"
@@ -149,36 +146,32 @@ export function DashboardNavbar() {
                   <Typography
                     variant="small"
                     color="blue-gray"
-                    className="mb-1 font-normal"
-                  >
-                    <strong>New album</strong> by Travis Scott
+                    className="mb-1 font-normal">
+                    <strong>New Full MCPs</strong> by Travis Scott
                   </Typography>
                   <Typography
                     variant="small"
                     color="blue-gray"
-                    className="flex items-center gap-1 text-xs font-normal opacity-60"
-                  >
-                    <ClockIcon className="h-3.5 w-3.5" /> 1 day ago
+                    className="flex items-center gap-1 text-xs font-normal opacity-60">
+                    <ClockIcon className="h-3.5 w-3.5" /> 1 mins ago
                   </Typography>
                 </div>
               </MenuItem>
               <MenuItem className="flex items-center gap-4">
                 <div className="grid h-9 w-9 place-items-center rounded-full bg-gradient-to-tr from-blue-gray-800 to-blue-gray-900">
-                  <CreditCardIcon className="h-4 w-4 text-white" />
+                  {/* <CreditCardIcon className="h-4 w-4 text-white" /> */}
                 </div>
                 <div>
                   <Typography
                     variant="small"
                     color="blue-gray"
-                    className="mb-1 font-normal"
-                  >
-                    Payment successfully completed
+                    className="mb-1 font-normal">
+                    Tasks successfully completed
                   </Typography>
                   <Typography
                     variant="small"
                     color="blue-gray"
-                    className="flex items-center gap-1 text-xs font-normal opacity-60"
-                  >
+                    className="flex items-center gap-1 text-xs font-normal opacity-60">
                     <ClockIcon className="h-3.5 w-3.5" /> 2 days ago
                   </Typography>
                 </div>
@@ -191,6 +184,6 @@ export function DashboardNavbar() {
   );
 }
 
-DashboardNavbar.displayName = "/src/widgets/layout/dashboard-navbar.jsx";
+DashboardNavbar.displayName = '/src/widgets/layout/dashboard-navbar.jsx';
 
 export default DashboardNavbar;
