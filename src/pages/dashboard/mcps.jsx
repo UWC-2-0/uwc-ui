efbda2dd5afcec9d8b679394d6061da132938cc3
issoe <twmnno@gmail.com>
2023-04-22 14:34:24 +0700

    merged

--- src/pages/dashboard/mcps.jsx
+++ src/pages/dashboard/mcps.jsx
@@ -1,19 +1,297 @@
-// import {
-//   Card,
-//   CardHeader,
-//   CardBody,
-//   Typography,
-//   Avatar,
-//   Chip,
-//   Tooltip,
-//   Progress,
-// } from '@material-tailwind/react';
-// import { EllipsisVerticalIcon } from '@heroicons/react/24/outline';
+import { Card, CardHeader, CardBody, Typography, Avatar, Chip, Tooltip, Progress, } from '@material-tailwind/react';
+import { EllipsisVerticalIcon } from '@heroicons/react/24/outline';
 // import { authorsTableData, projectsTableData } from '@/data';
+import mcpsData from '../../data/mcps-data';
+import React, { useEffect, useState } from 'react';
+import Posts from '../../components/posts';
+import Pagination from '../../components/Pagination'
+import { func } from 'prop-types';
+import {
+  Modal,
+  Ripple,
+  Input,
+  initTE,
+} from "tw-elements";
+
+initTE({ Modal, Ripple, Input });
 
 export function MCPs() {
+  const [posts, setPosts] = useState(mcpsData)
+  const [loading, setLoading] = useState(false)
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postsPerPage] = useState(10)
+
+
+  const indexOfLastPost = currentPage * postsPerPage
+  const indexOfFirstPost = indexOfLastPost - postsPerPage
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
+
+  const paginate = pageNumber => setCurrentPage(pageNumber)
+  const removeHandle = (index) => {
+    setPosts(posts.filter((post, idx) => idx !== index));
+  }
+
+  const [displayFormAdd, setDisplayFormAdd] = useState('none')
+
+  const current = new Date();
+  const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
+
+  const [formData, setFormData] = useState({ name: '', location: '' });
+  const [formData1, setFormData1] = useState({ name: '', location: '' });
+
+  const [idxCurrent, setIdxCurrent] = useState(0);
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleInputChange1 = (e) => {
+    setFormData1({ ...formData1, [e.target.name]: e.target.value });
+  }
+
+  const addHandle = () => {
+    setPosts([...posts, {
+      id: posts.length + 1,
+      checkbox: "uncheck",
+      name: formData1.name,
+      location: formData1.location,
+      date: date,
+      status: "New"
+    }])
+    setFormData1({ name: '', location: '' })
+  };
+
+  const updateHandle = () => {
+    posts[idxCurrent].name = formData.name
+    posts[idxCurrent].location = formData.location
+    setFormData({ name: '', location: '' })
+  }
+
+  const switchUpdating = (index, name, location) => {
+    setFormData({ name: name, location: location })
+    setIdxCurrent(index)
+  }
+
   return (
-    <div className="mt-12 mb-8 flex flex-col gap-12">MCPs Status here</div>
+    <div className="bg-white shadow-xl mt-12">
+      <button
+        type="button"
+        data-te-toggle="modal"
+        data-te-target="#exampleModalFullscreen1"
+        class="mt-2 ml-2 inline-block rounded mcp-bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+
+        CREATE MCP
+      </button>
+
+
+      <Posts posts={currentPosts} loading={loading} removeHandle={removeHandle} switchUpdating={switchUpdating} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} _paginate={paginate} />
+
+      <div
+        data-te-modal-init
+        class="fixed top-0 mt-12 z-[1055] hidden h-full w-4/12 overflow-y-auto overflow-x-hidden outline-none"
+        id="exampleModalFullscreen"
+        tabindex="-1"
+        aria-labelledby="exampleModalFullscreenLabel"
+        aria-hidden="true">
+        <div
+          data-te-modal-dialog-ref
+          className="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[0px]:m-0 min-[0px]:h-full min-[0px]:max-w-none">
+          <div
+            className="pointer-events-auto relative flex w-full flex-col rounded-md bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600 min-[0px]:h-full min-[0px]:rounded-none min-[0px]:border-0">
+            <div
+              className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50 min-[0px]:rounded-none">
+              <h5
+                className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
+                id="exampleModalFullscreenLabel">
+                Update MCP
+              </h5>
+              <button
+                type="button"
+                className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                data-te-modal-dismiss
+                aria-label="Close">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+
+
+            <div className="relative p-4 min-[0px]:overflow-y-auto">
+              <div className="flex justify-center">
+                <div className="block max-w-xl rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                  <div >
+                    <form onSubmit={(e) => {
+                      e.preventDefault();
+                      updateHandle();
+                    }}>
+                      <div className="ml-2">
+                        <h6 class="mb-2 mt-0 text-base font-medium leading-tight text-primary">Name</h6>
+                      </div>
+                      <div className="relative mb-4" data-te-input-wrapper-init>
+                        <input
+                          type="text"
+                          name="name"
+                          value={formData.name}
+                          onChange={handleInputChange}
+                          className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                          id="exampleFormControlInput1"
+                          placeholder="Name" />
+                      </div>
+                      <div className="ml-2">
+                        <h6 class="mb-2 mt-0 text-base font-medium leading-tight text-primary">Location</h6>
+                      </div>
+                      <div class="relative mb-4" data-te-input-wrapper-init>
+                        <input
+                          type="text"
+                          name="location"
+                          value={formData.location}
+                          onChange={handleInputChange}
+                          class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                          id="exampleFormControlInput1"
+                          placeholder="Location" />
+                      </div>
+                      <button
+                        type="submit"
+                        class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                        data-te-modal-dismiss>
+                        Update
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <div
+              className="mt-auto flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50 min-[0px]:rounded-none">
+              <button
+                type="button"
+                className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+                data-te-modal-dismiss>
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        data-te-modal-init
+        class="fixed top-0 mt-12 z-[1055] hidden h-full w-4/12 overflow-y-auto overflow-x-hidden outline-none"
+        id="exampleModalFullscreen1"
+        tabindex="-1"
+        aria-labelledby="exampleModalFullscreenLabel"
+        aria-hidden="true">
+        <div
+          data-te-modal-dialog-ref
+          className="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[0px]:m-0 min-[0px]:h-full min-[0px]:max-w-none">
+          <div
+            className="pointer-events-auto relative flex w-full flex-col rounded-md bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600 min-[0px]:h-full min-[0px]:rounded-none min-[0px]:border-0">
+            <div
+              className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50 min-[0px]:rounded-none">
+              <h5
+                className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
+                id="exampleModalFullscreenLabel">
+                New MCP
+              </h5>
+              <button
+                type="button"
+                className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                data-te-modal-dismiss
+                aria-label="Close">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+
+
+            <div className="relative p-4 min-[0px]:overflow-y-auto">
+              <div className="flex justify-center">
+                <div className="block max-w-xl rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                  <div >
+                    <form onSubmit={(e) => {
+                      e.preventDefault();
+                      addHandle();
+                    }}>
+                      <div className="ml-2">
+                        <h6 class="mb-2 mt-0 text-base font-medium leading-tight text-primary">Name</h6>
+                      </div>
+                      <div className="relative mb-4" data-te-input-wrapper-init>
+
+                        <input
+                          type="text"
+                          name="name"
+                          value={formData1.name}
+                          onChange={handleInputChange1}
+                          className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                          id="exampleFormControlInput1"
+                          placeholder="Name" />
+
+                      </div>
+                      <div className="ml-2">
+                        <h6 class="mb-2 mt-0 text-base font-medium leading-tight text-primary">Location</h6>
+                      </div>
+                      <div class="relative mb-4" data-te-input-wrapper-init>
+                        <input
+                          type="text"
+                          name="location"
+                          value={formData1.location}
+                          onChange={handleInputChange1}
+                          class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                          id="exampleFormControlInput1"
+                          placeholder="Location" />
+                      </div>
+
+
+
+                      <button
+                        type="submit"
+                        class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                        data-te-modal-dismiss>
+                        Add
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <div
+              className="mt-auto flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50 min-[0px]:rounded-none">
+              <button
+                type="button"
+                className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+                data-te-modal-dismiss>
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   );
 }
 
